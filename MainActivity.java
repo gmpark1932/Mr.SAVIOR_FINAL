bbc0ca3d94b1eedb727a435f32a47badbb434271
pgm <wnop123@gmail.com>
2018-11-28 07:07:15 +0900

    NaverAPI개발중

--- MainActivity.java
+++ MainActivity.java
@@ -6,8 +6,11 @@ package com.example.pgm.myapplication;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -23,11 +26,17 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "Main";
-
     Button btn_Insert;
     Button btn_send;
     Button btn_gps;
@@ -36,15 +45,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView text_Name;
     TextView text_Phone;
     TextView current_GPS;
+    TextView search_result;
 
     TextView tv;
     ToggleButton tb;
+
+    Button btn_next_page;
 
     long nowIndex;
     String name;
     long phone;
     String sort = "phone";
-
 
     ArrayAdapter<String> arrayAdapter;
 
@@ -52,10 +63,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     static ArrayList<String> arrayData = new ArrayList<String>();
     private DbOpenHelper mDbOpenHelper;
 
+    final Geocoder geocoder = new Geocoder(this);
+
+    List<Address> current_address;
+
+    public List<Address> getAddress(){
+        return current_address;
+    }
+
+    public static Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = this;
 
         //GPS 권한 요청
         checkVerify();
@@ -69,6 +92,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         current_GPS = (TextView)findViewById(R.id.current_gps);
         btn_gps = (Button) findViewById(R.id.gps_button);
         btn_gps.setOnClickListener(this);
+
+        search_result = (TextView)findViewById(R.id.search_result);
+
+        btn_next_page = (Button)findViewById(R.id.next_page_button);
+        btn_next_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,HospitalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Network를 위한
 
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         ListView listView = (ListView) findViewById(R.id.db_list);
@@ -131,11 +167,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             double altitude = location.getAltitude();   //고도
             float accuracy = location.getAccuracy();    //정확도
             String provider = location.getProvider();   //위치제공자
-            //Gps 위치제공자에 의한 위치변화. 오차범위가 좁다.
-            //Network 위치제공자에 의한 위치변화
-            //Network 위치는 Gps에 비해 정확도가 많이 떨어진다.
+            List<Address> list = null;
+            String address = null;
+
+            try {
+                list = geocoder.getFromLocation(
+                        latitude, // 위도
+                        longitude, // 경도
+                        10); // 얻어올 값의 개수
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("test", "입출력 오류 - 서버에서 주소변환시 에러발생");
+            }
+            if (list != null) {
+                if (list.size()==0) {
+                    address = "그런 주소는 없어요~";
+                } else {
+                    address = list.get(0).toString();
+                }
+            }
+
+            current_address = list;
+
             tv.setText("위치정보 : " + provider + "\n위도 : " + longitude + "\n경도 : " + latitude
-                    + "\n고도 : " + altitude + "\n정확도 : " + accuracy);
+                    + "\n고도 : " + altitude + "\n정확도 : " + accuracy
+            +"\n주소 : " + list.get(0).getAddressLine(0));
+            search_result.setText("하하하");
+
+            getHospitalList(0);
         }
 
         public void onProviderDisabled(String provider) {
@@ -157,7 +216,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void checkVerify()
     {
         if (
-                checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                        checkSelfPermission(Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED
                 )
         {
             // Should we show an explanation?
@@ -165,7 +225,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             {
                 // ...
             }
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.INTERNET},
                     1);
         }
         else
@@ -250,12 +311,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 arrayAdapter.addAll(arrayData);
                 arrayAdapter.notifyDataSetChanged();
                 break;
-            case R.id.gps_button:
-                current_GPS.setText("1111");
-                break;
         }
     }
 
 
+    final static String naverSearchURL = "https://openapi.naver.com/v1/search/local.json";
+    final static String naverClientID = "DRUQe9yF8wCGgKqgHlfY";
+    final static String naverClientSecret = "OQam5I5tiW";
 
+    public void getHospitalList(int count){
+        List<Address> address_list = ((MainActivity)MainActivity.mContext).getAddress();
+        String address = address_list.get(0).getAddressLine(0);
+        try {
+            String text = URLEncoder.encode("그린팩토리", "UTF-8");
+            String apiURL = "https://openapi.naver.com/v1/search/blog?query="+ text; // json 결과
+            //String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text; // xml 결과
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("X-Naver-Client-Id", naverClientID);
+            con.setRequestProperty("X-Naver-Client-Secret", naverClientSecret);
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 에러 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            System.out.println(response.toString());
+            search_result.setText(response.toString());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
 }
