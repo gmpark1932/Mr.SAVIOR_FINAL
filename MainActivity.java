575a282b07bf9dfe5f84974c28a5ad5f63905f67
pgm <wnop123@gmail.com>
2018-11-28 08:02:53 +0900

    동으로 검색하는 기능 완성

--- MainActivity.java
+++ MainActivity.java
@@ -5,6 +5,7 @@ package com.example.pgm.myapplication;
  */
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -14,6 +15,7 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
@@ -29,13 +31,24 @@ import android.widget.ToggleButton;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    final static String naverSearchURL = "https://openapi.naver.com/v1/search/local.json";
+    final static String naverClientID = "DRUQe9yF8wCGgKqgHlfY";
+    final static String naverClientSecret = "OQam5I5tiW";
+
+
     private static final String TAG = "Main";
     Button btn_Insert;
     Button btn_send;
@@ -73,6 +86,133 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public static Context mContext;
 
+    public class RequestHttpURLConnection {
+
+        public String request(String _url, ContentValues _params){
+
+            // HttpURLConnection 참조 변수.
+            HttpURLConnection urlConn = null;
+            // URL 뒤에 붙여서 보낼 파라미터.
+            StringBuffer sbParams = new StringBuffer();
+
+            /**
+             * 1. StringBuffer에 파라미터 연결
+             * */
+            // 보낼 데이터가 없으면 파라미터를 비운다.
+            if (_params == null)
+                sbParams.append("");
+                // 보낼 데이터가 있으면 파라미터를 채운다.
+            else {
+                // 파라미터가 2개 이상이면 파라미터 연결에 &가 필요하므로 스위칭할 변수 생성.
+                boolean isAnd = false;
+                // 파라미터 키와 값.
+                String key;
+                String value;
+
+                for(Map.Entry<String, Object> parameter : _params.valueSet()){
+                    key = parameter.getKey();
+                    value = parameter.getValue().toString();
+
+                    // 파라미터가 두개 이상일때, 파라미터 사이에 &를 붙인다.
+                    if (isAnd)
+                        sbParams.append("&");
+
+                    sbParams.append(key).append("=").append(value);
+
+                    // 파라미터가 2개 이상이면 isAnd를 true로 바꾸고 다음 루프부터 &를 붙인다.
+                    if (!isAnd)
+                        if (_params.size() >= 2)
+                            isAnd = true;
+                }
+            }
+
+            /**
+             * 2. HttpURLConnection을 통해 web의 데이터를 가져온다.
+             * */
+            try{
+                URL url = new URL(_url);
+                urlConn = (HttpURLConnection) url.openConnection();
+
+                // [2-1]. urlConn 설정.
+                urlConn.setRequestMethod("POST"); // URL 요청에 대한 메소드 설정 : POST.
+                urlConn.setRequestProperty("Accept-Charset", "UTF-8"); // Accept-Charset 설정.
+                urlConn.setRequestProperty("Context_Type", "application/x-www-form-urlencoded;cahrset=UTF-8");
+
+                // [2-2]. parameter 전달 및 데이터 읽어오기.
+                String strParams = sbParams.toString(); //sbParams에 정리한 파라미터들을 스트링으로 저장. 예)id=id1&pw=123;
+                OutputStream os = urlConn.getOutputStream();
+                os.write(strParams.getBytes("UTF-8")); // 출력 스트림에 출력.
+                os.flush(); // 출력 스트림을 플러시(비운다)하고 버퍼링 된 모든 출력 바이트를 강제 실행.
+                os.close(); // 출력 스트림을 닫고 모든 시스템 자원을 해제.
+
+                // [2-3]. 연결 요청 확인.
+                // 실패 시 null을 리턴하고 메서드를 종료.
+                if (urlConn.getResponseCode() != HttpURLConnection.HTTP_OK)
+                    return null;
+
+                // [2-4]. 읽어온 결과물 리턴.
+                // 요청한 URL의 출력물을 BufferedReader로 받는다.
+                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConn.getInputStream(), "UTF-8"));
+
+                // 출력물의 라인과 그 합에 대한 변수.
+                String line;
+                String page = "";
+
+                // 라인을 받아와 합친다.
+                while ((line = reader.readLine()) != null){
+                    page += line;
+                }
+
+                return page;
+
+            } catch (MalformedURLException e) { // for URL.
+                e.printStackTrace();
+            } catch (IOException e) { // for openConnection().
+                e.printStackTrace();
+            } finally {
+                if (urlConn != null)
+                    urlConn.disconnect();
+            }
+
+            return null;
+
+        }
+
+    }
+
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask(String url, ContentValues values) {
+
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String result; // 요청 결과를 저장할 변수.
+            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+
+            result = getHospitalList(0);
+
+            //result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+            search_result.setText(s);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -104,7 +244,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        //Network를 위한
+
 
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         ListView listView = (ListView) findViewById(R.id.db_list);
@@ -192,9 +332,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             tv.setText("위치정보 : " + provider + "\n위도 : " + longitude + "\n경도 : " + latitude
                     + "\n고도 : " + altitude + "\n정확도 : " + accuracy
             +"\n주소 : " + list.get(0).getAddressLine(0));
-            search_result.setText("하하하");
+            ContentValues cv = new ContentValues();
+            cv.put("X-Naver-Client-Id",naverClientID);
+            cv.put("X-Naver-Client-Secret", naverClientSecret);
 
-            getHospitalList(0);
+            NetworkTask networkTask = new NetworkTask(naverSearchURL, cv);
+            networkTask.execute();
+
+            //getHospitalList(0);
         }
 
         public void onProviderDisabled(String provider) {
@@ -315,16 +460,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    final static String naverSearchURL = "https://openapi.naver.com/v1/search/local.json";
-    final static String naverClientID = "DRUQe9yF8wCGgKqgHlfY";
-    final static String naverClientSecret = "OQam5I5tiW";
-
-    public void getHospitalList(int count){
+    public String getHospitalList(int count){
         List<Address> address_list = ((MainActivity)MainActivity.mContext).getAddress();
-        String address = address_list.get(0).getAddressLine(0);
+        String address = address_list.get(0).getThoroughfare();
+        String result = new String();
         try {
-            String text = URLEncoder.encode("그린팩토리", "UTF-8");
-            String apiURL = "https://openapi.naver.com/v1/search/blog?query="+ text; // json 결과
+            String text = URLEncoder.encode(address + " 응급실", "UTF-8");
+            String apiURL = "https://openapi.naver.com/v1/search/local.xml?query="+ text; // json 결과
             //String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text; // xml 결과
             URL url = new URL(apiURL);
             HttpURLConnection con = (HttpURLConnection)url.openConnection();
@@ -345,10 +487,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
             br.close();
             System.out.println(response.toString());
-            search_result.setText(response.toString());
+            result = response.toString();
+            //search_result.setText(response.toString());
         } catch (Exception e) {
             System.out.println(e);
         }
 
+        return result;
     }
 }
